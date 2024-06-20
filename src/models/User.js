d772fe78bdae466c85381af7181c1968e5e8387e
IsabelaSouto <belasoutomaior@gmail.com>
2024-06-20 19:58:56 -0300

    req 2 done

---
+++ src/models/User.js
@@ -0,0 +1,17 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    display_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  })
+
+  return User;
+};
+
+module.exports = UserModel;
