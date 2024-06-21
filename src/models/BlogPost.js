b8bd84379a85d848021dc80296ca41451df978ee
IsabelaSouto <belasoutomaior@gmail.com>
2024-06-21 20:21:01 -0300

    req 10 done

---
+++ src/models/BlogPost.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  }, {
+    tableName: 'BlogPosts',
+    timestamps: false,
+    underscored: true,
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, { as: 'user', foreignKey: 'userId' });
+  };
+
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -16,5 +16,9 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: false,
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, { as: 'blogPosts', foreignKey: 'userId' });
+  };
+
   return User;
 };
