90077104643cf41793d35a3be1f439b71d0a6d3e
IsabelaSouto <belasoutomaior@gmail.com>
2024-06-21 20:34:15 -0300

    req 11 done

---
+++ src/models/PostCategory.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  }, {
+    tableName: 'posts_categories',
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'post_id',
+      otherKey: 'category_id',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'category_id',
+      otherKey: 'post_id',
+    });
+  };
+
+  return PostCategory;
+};
